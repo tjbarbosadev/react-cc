523fa0e96a32d032538acb14bf88ab459dbfbdee
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-04-22 09:25:15 -0300

    Working with useState to manipulate descrption size

--- react-cc/src/components/JobListing.jsx
+++ react-cc/src/components/JobListing.jsx
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+
 const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+  if (!showFullDescription) {
+    description = description.substring(0, 90).trim() + '...';
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -7,7 +16,16 @@ const JobListing = ({ job }) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-5">
+          {description}
+          <br />
+          <button
+            onClick={() => setShowFullDescription((prevState) => !prevState)}
+            className="text-indigo-500 mb-5 hover:text-indigo-600"
+          >
+            {showFullDescription ? 'less' : 'more'}
+          </button>
+        </div>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
