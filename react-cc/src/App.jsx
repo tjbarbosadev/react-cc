ba0d49fadb84766a8d60fb809e010517d667124c
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-04-22 10:39:30 -0300

    Configuring routes with "Router DOM"

--- react-cc/src/App.jsx
+++ react-cc/src/App.jsx
@@ -1,27 +1,23 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <>
-      <Navbar />
-
-      {/* <!-- Hero --> */}
-      <Hero />
-
-      {/* <!-- Developers and Employers --> */}
-      <HomeCards />
-
-      {/* <!-- Browse Jobs --> */}
-      <JobListings />
-
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
