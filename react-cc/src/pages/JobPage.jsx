6a41b0e63a16d542c74a1587c1f2d3ddd9dfb581
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-04-22 13:26:21 -0300

    Refactoring job loading data with "useLoaderData"

--- react-cc/src/pages/JobPage.jsx
+++ react-cc/src/pages/JobPage.jsx
@@ -1,29 +1,15 @@
-import { useState, useEffect } from 'react';
-import Spinner from '../components/Spinner';
-import { useParams } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
 const JobPage = () => {
-  const { id } = useParams();
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const job = useLoaderData();
 
-  useEffect(() => {
-    const fetchJob = async () => {
-      try {
-        const res = await fetch(`/api/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchJob();
-  }, []);
-
-  return loading ? <Spinner /> : <div>{job.title}</div>;
+  return <div>{job.title}</div>;
 };
 
-export default JobPage;
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
